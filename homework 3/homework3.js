574bae8f80dac3772c4f3916773c906b52c36061
SonyaStelmah <78246853+SonyaStelmah@users.noreply.github.com>
2021-02-05 22:53:49 +0200

    Add files via upload

--- homework 3/homework3.js
+++ homework 3/homework3.js
@@ -1,3 +1,4 @@
+
 //task 1
 let result = 1;
 let arr = [2, 3, 4, 5];
@@ -13,7 +14,7 @@ while (counter < arr.length) {
     res *= arr[counter];
     counter += 1;
 };
-console.log(res);
+console.log(res); 
 
 //task 2 
 let a = 0
@@ -56,24 +57,36 @@ function findMin(input) {
 
 
 //task 6 
-function findUnique(arr)
-{   
-    let n = arr.length;   
-    for (let i = 1; i < n; i++)
-      { if (arr[i-1] === arr[ i ]) return false; } 
+function findUnique(arr){    
+    for (let i = 0; i < arr.length; i++){
+        for(let j = (i+1); j < arr.length; j++){
+            if(arr[i] === arr[j]){
+                return false;
+            }
+        }
+    }
+      
     return true;
 }
+console.log(findUnique([2, 4, 8, 8, 9]));
 
 //task 7
-function theLastOne(arr) {
-    return arr[arr.length - 1];
+function lastElem(arr, count) {
+  if (count == null) {
+      console.log(arr.pop())
+  }
+  else {
+      console.log(arr.slice(-count));
+  }
 }
-console.log(theLastOne([3, 4, 7, 8]))
-
+  console.log(lastElem([2, 3, 11, -7]));
+  console.log(lastElem([2, 3, 11, -7], 2));
+  console.log(lastElem([2, 3, 11, -7], 8));
 //task 8 
 let string = 'I love java script';
 
 console.log(
 string.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ')
 );
+
 
