c22a74487bdd43158fb3b93243213a8d6db8254c
SonyaStelmah <78246853+SonyaStelmah@users.noreply.github.com>
2021-02-10 01:13:34 +0200

    Add files via upload

--- homework 4/hw4.js
+++ homework 4/hw4.js
@@ -18,7 +18,7 @@ try {
         let userAge = prompt("Enter your age", "");
         if (userAge < 14) {
             throw new Error("You are too young");
-        } else if (isNaN(userAge)) {
+        } else if (isNaN(parseInt(userAge))) {
             throw new Error("This is not a number");
         } else if (userAge == null) {
             throw new Error ("This field is empty");
@@ -32,16 +32,50 @@ try {
      console.log(exception.stack);
  }
 
+ // task 3
+ class MonthException {
+    constructor(message) {
+        this.message = message;
+        this.name = 'MonthException';
+    }
+ }
+ function showMonthName(monthNumber) {
+     monthNumber = monthNumber - 1;
+     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+     if (months[monthNumber] !== undefined) {
+         return months[monthNumber];
+     } else {
+         throw new MonthException('Wrong month number');
+     }
+ }
+ try {
+     let someMonth = 17;
+     let monthName = showMonthName(someMonth);
+     console.log(monthName);
+ } catch(exception) {
+     console.error(exception.name, exception.message);
+ }
+
  //task 4
  function ShowUserId (id){
     this.id= id;
 if (id < 0){
-throw new Error("Must not be negative");
+throw new Error("Must not be negative" + id); 
 }
+return {id: id};
 }
-let name = new ShowUserId ("Sonya");
 
 
-function ShowUsers(ids) {
-    let ids = [];
+function ShowUsersIds(ids) {
+    let result = [];
+    ids.forEach(function(id){
+        try {
+            let user = ShowUserId(id);
+            result.push(user);   
+        } catch (exception) {
+         console.log(exception.message);
+        }
+    });
+    return result;
 }
+ShowUsersIds([2, -8, 44, 58]);
