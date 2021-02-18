caf70fbb403ee74677db7e28d8b8d22f4dce78bb
SonyaStelmah <78246853+SonyaStelmah@users.noreply.github.com>
2021-02-18 17:33:54 +0200

    Add files via upload

---
+++ homework7/hw7.js
@@ -0,0 +1,28 @@
+//task 1
+// let x = window.open("", "","width = 300, height = 300, left = 700, top = 200");
+
+
+// setTimeout(function(){ 
+//     x.resizeTo(500, 500);
+       
+// }, 2000);
+
+// setTimeout(function (){ 
+//     x.moveTo(200, 200);
+       
+// }, 2000);
+
+//    setTimeout(function (){
+//    x.close();
+//    }, 2000);
+   
+//task 2
+
+let text = document.getElementById("text");
+
+function changeCSS(){
+text.style.color = "orange"
+text.style.fontSize = "20px"
+text.style.fontFamily = "Comic "
+
+}
